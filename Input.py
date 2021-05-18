3355231b95524bd324d089cf0da82dd77316fee5
gpierobon <77953607+gpierobon@users.noreply.github.com>
2021-05-18 18:06:28 +1000

    Update Input.py

--- Input.py
+++ Input.py
@@ -7,32 +7,9 @@ fa_phys = 10**12.0 # Phsycal value of fa in GeV
 lambdaPRS = 1 # Quartic coupling 
 Potential = 'Mexican' # Choose potential. Options: 'MexicanHat' or 'Thermal'
 
-N = 150 # Number of grid points
+N = 256 # Number of grid points
 Delta = 1 # Default Delta=1 and H=fa
 DeltaRatio = 1.0/3.0 # Time/Space step ratio 
 Era = 1 # Era=1 for radiation domination, Era=2 for early matter domination (in PRS trick)
 StencilOrder = 2 
 
-#######################################
-# INDEPENDENT PARAMETERS 
-#######################################
-
-# from numpy import *
-# Mpl = 2.4*10**18.0/fa_phys # Normalized to fa_phys chosen 
-# fa = 1 # In fa units
-# ms = sqrt(lambdaPRS)*fa # Saxion mass for PRS in ADM units
-# L = Delta*N # Comoving 
-# Delta_tau = DeltaRatio*Delta
-# H1 = fa/Delta
-
-# # Code spacings
-# dx = ms*Delta 
-# dtau = ms*Delta_tau
-# t_evol = dtau/DeltaRatio - dtau # Such that in the first loop iteration, t_evol = 1 
-# light_time = int(0.5*N/DeltaRatio)
-# gstar = 106.75
-# T0 = math.sqrt(Mpl*90.0/(gstar*pi*pi)) # In units of fa
-# R0 = Delta*ms/(ms*L)
-# t0 = Delta/(2*L*ms**2.0)
-# meffsquared = lambdaPRS**2.0*(T0**2.0/3.0-1)
-        
