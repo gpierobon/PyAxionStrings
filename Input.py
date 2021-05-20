b5009ef9f797ade1cc33cc00f3744cbf69338a02
gpierobon <77953607+gpierobon@users.noreply.github.com>
2021-05-20 12:43:42 +1000

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,34 +1,36 @@
-#######################################
-# Input.py ############################
-#######################################
+#=================================================================================================================
+# Input.py 
+# PyAxions (2021)
+#=================================================================================================================
 
 NDIMS = 3 # Spatial dimensions of the simulation box. Set NDIMS = 2 or 3
 fa_phys = 10**12.0 # Phsycal value of fa in GeV
 lambdaPRS = 1 # Quartic coupling 
-Potential = 'Mexican' # Choose potential. Options: 'MexicanHat' or 'Thermal'
+Potential = 'Thermal' # Choose potential. Options: 'MexicanHat' or 'Thermal'
 
 N = 64 # Number of grid points
 Delta = 1 # Default Delta=1 and H=fa
 DeltaRatio = 1.0/3.0 # Time/Space step ratio 
 Era = 1 # Era=1 for radiation domination, Era=2 for early matter domination (in PRS trick)
-StencilOrder = 2 
+StencilOrder = 2
+time_var = 'Conformal time' 
 
-#--------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 # SNAPSHOTS
-#--------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 
-print_snapshot_final = True
+print_snapshot_final = False
 
 f_axion = True
 f_saxion = False
 f_double = False
-f_3dstrings = False
+f_3dstrings = True
 
-#--------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 # ANALYSIS
-#--------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 
-analyse_strings = False
+analyse_strings = True
 analyse_spectrum = False
 analyse_energies = False
 
