bbbf4c885df1eb118b2d1d9c1666d0f332f54bce
gpierobon <gpierobon@users.noreply.github.com>
2022-04-28 18:05:06 +1000

    Default to 128

--- Input.py
+++ Input.py
@@ -7,7 +7,7 @@ fa_phys = 10**12.0 # Phsycal value of fa in GeV
 lambdaPRS = 1 # Quartic coupling 
 Potential = 'Thermal' # Choose potential. Options: 'MexicanHat' or 'Thermal'
 
-N = 256 # Number of grid points
+N = 128 # Number of grid points
 Delta = 1 # Default Delta=1 and H=fa
 DeltaRatio = 1.0/3.0 # Time/Space step ratio 
 Era = 1 # Era=1 for radiation domination, Era=2 for early matter domination (in PRS trick)
